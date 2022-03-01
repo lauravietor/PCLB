934c30d2cdc4e47c73df9c0ed60f00c53dec349d
Gudf <paul.vietor@centrale-marseille.fr>
2022-03-01 19:35:59 +0100

    Connexion via MyCA

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,11 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -57,6 +63,70 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
+
+    }
+
+    /**
+     * @Route("/login/oauth", name="security_oauth")
+     */
+    public function oauth(Request $request, TokenStorageInterface $tokenStorage, SessionInterface $session, EventDispatcherInterface $dispatcher, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder)
+    {
+        if($this->isGranted('ROLE_USER')){
+            return $this->redirectToRoute('security_login');
+        }
+
+        $id = $this->getParameter('oauth_id');
+        $secret = $this->getParameter('oauth_secret');
+        $base = $this->getParameter('oauth_base');
+
+        $client = new \OAuth2\Client($id, $secret);
+
+        if(!$request->query->has('code')){
+            $url = $client->getAuthenticationUrl($base.'/oauth/v2/auth', $this->generateUrl('security_oauth', [],UrlGeneratorInterface::ABSOLUTE_URL));
+            return $this->redirect($url);
+        }else{
+            $params = ['code' => $request->query->get('code'), 'redirect_uri' => $this->generateUrl('security_oauth', [],UrlGeneratorInterface::ABSOLUTE_URL)];
+            $resp = $client->getAccessToken($base.'/oauth/v2/token', 'authorization_code', $params);
+
+            if(isset($resp['result']) && isset($resp['result']['access_token'])){
+                $info = $resp['result'];
+
+                $client->setAccessTokenType(\OAuth2\Client::ACCESS_TOKEN_BEARER);
+                $client->setAccessToken($info['access_token']);
+                $response = $client->fetch($base.'/api/user/me');
+                $data = $response['result'];
+
+                $username = $data['username'];
+
+                $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['username'=>$username]);
+                if($user === null){ // Création de l'utilisateur s'il n'existe pas
+                    $user = new User;
+                    $user->setUsername($username);
+                    $user->setEmail($data['email']);
+                    $user->setLastName($data['nom']);
+                    $user->setFirstName($data['prenom']);
+                    $user->setRoles(["ROLE_USER"]);
+                    $user->setScore(0);
+                    $user->setPassword($encoder->encodePassword($user, uniqid()));
+
+                    $manager->persist($user);
+                    $manager->flush();
+                }
+
+                // Connexion effective de l'utilisateur
+                $token = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
+                $tokenStorage->setToken($token);
+
+                $session->set('_security_main', serialize($token));
+
+                $event = new InteractiveLoginEvent($request, $token);
+                $dispatcher->dispatch("security.interactive_login", $event);
+
+            }
+
+            // Redirection vers l'accueil
+            return $this->redirectToRoute('security_login');
+        }
 
     }
 }
